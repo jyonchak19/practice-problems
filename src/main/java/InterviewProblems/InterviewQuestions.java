5fb011c35e4b06cf8ce028496fa2ed26e8dc75c3
Jacob <jyonchak19@gmail.com>
2022-09-16 11:01:06 -0500

    Optimized maxSubArray and worked on minSubArray, maxSubArrayRecursive, and maxSubArraySliding.

--- src/main/java/InterviewProblems/InterviewQuestions.java
+++ src/main/java/InterviewProblems/InterviewQuestions.java
@@ -10,6 +10,7 @@ public class InterviewQuestions {
     }
 
 
+    // ***************** KADANE'S ALGORITHM ***********************
     // given int arr nums, find the contiguous subarray
     // (must contain at least 1 number) that has the largest
     // sum, and return that specific sum.
@@ -20,12 +21,56 @@ public class InterviewQuestions {
     // nums = [5, 4, -1, 7, 8]
     // output: 23
     public static int maxSubArray(int[] nums) {
-        int currentSum, currentMax = Integer.MIN_VALUE;
+        int maxLastSeen = nums[0], maxEndedHere = nums[0];
+        for(int i = 1; i < nums.length;  i++) {
+            maxEndedHere = Math.max(maxEndedHere + nums[i], nums[i]);
+            maxLastSeen = Math.max(maxLastSeen, maxEndedHere);
+        }
+        return maxLastSeen;
+    }
+
+    public static int maxSubArrayBrute(int[] nums) {
+        int currentSum = 0, currentMax = Integer.MIN_VALUE;
         for(int i = 0; i < nums.length;  i++) {
-            currentSum = 0;
-            for(int j = i; j < nums.length; j++) {
+           for(int j = i; j < nums.length; j++) {
+               currentSum = currentSum + nums[j];
+               if(currentSum > currentMax)
+                   currentMax = currentSum;
+            }
+        }
+        return currentMax;
+    }
+
+    public static int minSubArray(int[] nums) {
+        int minLastSeen = nums[0], minEndedHere = nums[0];
+        for(int i = 1; i < nums.length;  i++) {
+            minEndedHere = Math.min(minEndedHere + nums[i], nums[i]);
+            minLastSeen = Math.min(minLastSeen, minEndedHere);
+        }
+        return minLastSeen;
+    }
+
+    public static int maxSubArrayRecursive(int[] nums) {
+        int maxLastSeen = nums[0], maxEndedHere = nums[0];
+        return maxSubArrayRecursiveHelper(nums, maxLastSeen, maxEndedHere, 1);
+    }
+    public static int maxSubArrayRecursiveHelper(int[] nums, int maxLastSeen, int maxEndedHere, int i) {
+        if(i == nums.length)
+            return maxLastSeen;
+        maxEndedHere = Math.max(maxEndedHere + nums[i], nums[i]);
+        maxLastSeen = Math.max(maxLastSeen, maxEndedHere);
+        return maxSubArrayRecursiveHelper(nums, maxLastSeen, maxEndedHere, i + 1);
+    }
+
+    // find the maximum possible contiguous sum with k elements
+
+    // [1,3,-1,-3,5,3,6,7] k = 3
+    public static int maxSubArraySliding(int[] nums, int k) {
+        int currentSum = 0, currentMax = Integer.MIN_VALUE;
+        for(int i = 0; i < nums.length;  i++) {
+            for(int j = i; j + k < nums.length; j++) {
                 currentSum = currentSum + nums[j];
-                if(currentMax < currentSum)
+                if(currentSum > currentMax && j - i + 1 == k)
                     currentMax = currentSum;
             }
         }
--- src/test/java/InterviewQuestions/InterviewQuestionsTest.java
+++ src/test/java/InterviewQuestions/InterviewQuestionsTest.java
@@ -6,10 +6,39 @@ import main.java.InterviewProblems.InterviewQuestions;
 
 public class InterviewQuestionsTest {
     @Test
-    public void maxSubArrayTest() {
+    public void maxSubArrayTest1() {
         int[] input = {5, 4, -1, 7, 8};
         int expectedResult = 23;
         Assert.assertEquals(expectedResult,
                 InterviewQuestions.maxSubArray(input));
+    }
+    @Test
+    public void maxSubArrayTest2() {
+        int[] input = {-2, -1, -3, 4, -1, 2, 1, -5, 4};
+        int expectedResult = 6;
+        Assert.assertEquals(expectedResult,
+                InterviewQuestions.maxSubArray(input));
+    }
+    @Test
+    public void maxSubArrayTest3() {
+        int[] input = {-5, -7, -3, -1, -9, -2};
+        int expectedResult = -1;
+        Assert.assertEquals(expectedResult,
+                InterviewQuestions.maxSubArray(input));
+    }
+    @Test
+    public void maxSubArrayRecursiveTest1() {
+        int[] input = {-2, -1, -3, 4, -1, 2, 1, -5, 4};
+        int expectedResult = 6;
+        Assert.assertEquals(expectedResult,
+                InterviewQuestions.maxSubArrayRecursive(input));
+    }
+    @Test
+    public void maxSubArraySlidingTest1() {
+        int[] arr = {1,3,-1,-3,5,3,6,7};
+        int k = 3;
+        int expectedResult = 16;
+        Assert.assertEquals(expectedResult,
+                InterviewQuestions.maxSubArraySliding(arr, k));
     }
 }
